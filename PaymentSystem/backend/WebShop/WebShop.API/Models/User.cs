c0c8570cad357d883675fc8af9cf4e19f33ea7d3
katarinap02 <148916553+katarinap02@users.noreply.github.com>
2025-12-20 18:48:04 +0100

    added class

---
+++ PaymentSystem/backend/WebShop/WebShop.API/Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace WebShop.API.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public User() { }   
+
+        public User(long id, string email, string passwordHash, string name, string surname)
+        {
+            Id = id;
+            Email = email;
+            PasswordHash = passwordHash;
+            Name = name;
+            Surname = surname;
+
+        }
+    }
+}
